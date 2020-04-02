cff7e0be6a5d6b0c66f5705199301cadfbf6ec8f
Peter Rufo <peter@peterrufo.com>
2020-04-02 10:43:52 -0400

    updates

--- src/Components/RecipeListItem/RecipeListItem.js
+++ src/Components/RecipeListItem/RecipeListItem.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './RecipeListItem.css'
 
 export default function RecipeListItem(props) {
   return (
     <div className="recipe-list-item">
       <h3><Link to={`/recipes/${props.data.id}`}>{props.data.title}</Link></h3>
       <p>{props.data.source}</p>
+      <img src={props.data.photo_url} alt='recipe thumbnail' className='thumbnail'/>
+     
     </div>
   )
 }
